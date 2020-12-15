de808be42ba42bf06b7c6f7c25d8bad92e84e316
Avijit Karmaker <ajkacca@gmail.com>
2020-12-16 00:14:33 +0200

    image folder add

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    				rules: [
+   				   {
+        				test: /\.css$/i,
+        				use: ['style-loader', 'css-loader'],
+      				},
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          'file-loader',
+        ],
+      },
+    				],
+  				},
+  watch: true,
+};
