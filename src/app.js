857986bb448a54f5bde0b601cf6f9c49647699a7
Avijit Karmaker <ajkacca@gmail.com>
2020-12-16 15:15:40 +0200

    add ferclear

--- src/app.js
+++ src/app.js
@@ -10,20 +10,34 @@ const Initialize = () => {
   const display = document.querySelector('.display');
   const error = document.querySelector('.errordisplay');
   const clearbtn = document.querySelector('#btn2');
+  const loader=document.querySelector("#load");
 
   const Showerror = (msg) => {
     error.innerHTML = `${msg}`;
     error.classList.add('show');
   };
 
-  const Showdata = (data) => {
-    if (data.message === 'city not found') {
-      Showerror('City doesnt exists.. Please enter valid city name.');
-      setTimeout(() => {
-        error.classList.remove('show');
-      }, 2000);
-    } else {
-      display.innerHTML = `
+  const showloader=()=>{
+  loader.classList.add("load");
+  display.classList.add("load");
+  }
+
+  const showdata= (data)=>{
+
+  if(data.message==="city not found"){
+    showerror("City doesnt exists.. Please enter valid city name.");
+    setTimeout(()=>{
+      error.classList.remove("show");
+    }, 2000);
+  }
+  else {
+  showloader();
+  setTimeout(()=>{
+    loader.classList.remove("load");
+    display.classList.remove("load");
+  }, 1000);
+
+  display.innerHTML= `
   <hr>
   <div class="top">
   <div class="country">
@@ -45,22 +59,54 @@ const Initialize = () => {
 
 
   <div class="other">
-  <h3 class="white-color">Feels like : ${Math.round(data.main.feels_like)}&deg; &#124;
+
+  <div class="btncont">
+  <button id="cel">C</button>
+  <button id="fer">F</button>
+  </div>
+
+  <h3 class="white-color">Feels like : <span id="feel">${Math.round(data.main.feels_like)}</span>&deg; &#124;
   Air Pressure : ${data.main.pressure} millibars &#124;
   Humidity : ${data.main.humidity}%</h3>
 
-  <h3 class="white-color"> Max Temperature : ${Math.round(data.main.temp_max)}&deg; &#124;
-  Min Temperature : ${Math.round(data.main.temp_min)}&deg;
+  <h3 class="white-color"> Max Temperature : <span id="maxtemp">${Math.round(data.main.temp_max)}</span>&deg; &#124;
+  Min Temperature : <span id="mintemp">${Math.round(data.main.temp_min)}</span>&deg;
   </h3>
 
-  <h3 class="white-color">Time of Sunrise : ${Timeconvert(data.sys.sunrise)} &#124;
-  Time of Sunset : ${Timeconvert(data.sys.sunset)}</h3>
+  <h3 class="white-color">Time of Sunrise : ${timeconvert(data.sys.sunrise)} &#124;
+  Time of Sunset : ${timeconvert(data.sys.sunset)}</h3>
 
   <h3 class="white-color">Wind Speed : ${data.wind.speed} m/s &#124 Wind Degree : ${data.wind.deg}&deg;</h3>
   </div>
-  `;
-    }
-  };
+  `
+
+  const cel=document.querySelector("#cel");
+  const fer=document.querySelector("#fer");
+
+  const temp=document.querySelector("#temp");
+  const mintemp=document.querySelector("#mintemp");
+  const mtemp=document.querySelector("#maxtemp");
+  const feel=document.querySelector("#feel");
+
+  cel.addEventListener("click",()=>{
+    cel.classList.add("active");
+    fer.classList.remove("active");
+    temp.innerHTML=`${Math.round(data.main.temp)}&deg`;
+    mintemp.innerHTML=`${Math.round(data.main.temp_min)}`;
+    maxtemp.innerHTML=`${Math.round(data.main.temp_max)}`;
+    feel.innerHTML=`${Math.round(data.main.feels_like)}`;
+  })
+
+  fer.addEventListener("click",()=>{
+    fer.classList.add("active");
+    cel.classList.remove("active");
+    temp.innerHTML=`${converttemp(data.main.temp)}&deg`;
+    mintemp.innerHTML=`${converttemp(data.main.temp_min)}`;
+    maxtemp.innerHTML=`${converttemp(data.main.temp_max)}`;
+    feel.innerHTML=`${converttemp(data.main.feels_like)}`;
+  })
+  }
+  }
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
