78f1c9b810ea0da89b1c2e833191b6dd1aebad60
Avijit Karmaker <ajkacca@gmail.com>
2020-12-16 15:19:46 +0200

    fix linters

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import Getweather from './getweather';
 import Timeconvert from './timeconvert';
 import Interface from './interface';
+import Converttemp from './converttemp';
 
 const Initialize = () => {
   Interface();
@@ -10,34 +11,32 @@ const Initialize = () => {
   const display = document.querySelector('.display');
   const error = document.querySelector('.errordisplay');
   const clearbtn = document.querySelector('#btn2');
-  const loader=document.querySelector("#load");
+  const loader = document.querySelector('#load');
 
   const Showerror = (msg) => {
     error.innerHTML = `${msg}`;
     error.classList.add('show');
   };
 
-  const showloader=()=>{
-  loader.classList.add("load");
-  display.classList.add("load");
-  }
+  const Showloader = () => {
+    loader.classList.add('load');
+    display.classList.add('load');
+  };
 
-  const showdata= (data)=>{
+  const Showdata = (data) => {
+    if (data.message === 'city not found') {
+      Showerror('City doesnt exists.. Please enter valid city name.');
+      setTimeout(() => {
+        error.classList.remove('show');
+      }, 2000);
+    } else {
+      Showloader();
+      setTimeout(() => {
+        loader.classList.remove('load');
+        display.classList.remove('load');
+      }, 1000);
 
-  if(data.message==="city not found"){
-    showerror("City doesnt exists.. Please enter valid city name.");
-    setTimeout(()=>{
-      error.classList.remove("show");
-    }, 2000);
-  }
-  else {
-  showloader();
-  setTimeout(()=>{
-    loader.classList.remove("load");
-    display.classList.remove("load");
-  }, 1000);
-
-  display.innerHTML= `
+      display.innerHTML = `
   <hr>
   <div class="top">
   <div class="country">
@@ -73,40 +72,40 @@ const Initialize = () => {
   Min Temperature : <span id="mintemp">${Math.round(data.main.temp_min)}</span>&deg;
   </h3>
 
-  <h3 class="white-color">Time of Sunrise : ${timeconvert(data.sys.sunrise)} &#124;
-  Time of Sunset : ${timeconvert(data.sys.sunset)}</h3>
+  <h3 class="white-color">Time of Sunrise : ${Timeconvert(data.sys.sunrise)} &#124;
+  Time of Sunset : ${Timeconvert(data.sys.sunset)}</h3>
 
   <h3 class="white-color">Wind Speed : ${data.wind.speed} m/s &#124 Wind Degree : ${data.wind.deg}&deg;</h3>
   </div>
-  `
+  `;
 
-  const cel=document.querySelector("#cel");
-  const fer=document.querySelector("#fer");
+      const cel = document.querySelector('#cel');
+      const fer = document.querySelector('#fer');
 
-  const temp=document.querySelector("#temp");
-  const mintemp=document.querySelector("#mintemp");
-  const mtemp=document.querySelector("#maxtemp");
-  const feel=document.querySelector("#feel");
+      const temp = document.querySelector('#temp');
+      const mintemp = document.querySelector('#mintemp');
+      const maxtemp = document.querySelector('#maxtemp');
+      const feel = document.querySelector('#feel');
 
-  cel.addEventListener("click",()=>{
-    cel.classList.add("active");
-    fer.classList.remove("active");
-    temp.innerHTML=`${Math.round(data.main.temp)}&deg`;
-    mintemp.innerHTML=`${Math.round(data.main.temp_min)}`;
-    maxtemp.innerHTML=`${Math.round(data.main.temp_max)}`;
-    feel.innerHTML=`${Math.round(data.main.feels_like)}`;
-  })
+      cel.addEventListener('click', () => {
+        cel.classList.add('active');
+        fer.classList.remove('active');
+        temp.innerHTML = `${Math.round(data.main.temp)}&deg`;
+        mintemp.innerHTML = `${Math.round(data.main.temp_min)}`;
+        maxtemp.innerHTML = `${Math.round(data.main.temp_max)}`;
+        feel.innerHTML = `${Math.round(data.main.feels_like)}`;
+      });
 
-  fer.addEventListener("click",()=>{
-    fer.classList.add("active");
-    cel.classList.remove("active");
-    temp.innerHTML=`${converttemp(data.main.temp)}&deg`;
-    mintemp.innerHTML=`${converttemp(data.main.temp_min)}`;
-    maxtemp.innerHTML=`${converttemp(data.main.temp_max)}`;
-    feel.innerHTML=`${converttemp(data.main.feels_like)}`;
-  })
-  }
-  }
+      fer.addEventListener('click', () => {
+        fer.classList.add('active');
+        cel.classList.remove('active');
+        temp.innerHTML = `${Converttemp(data.main.temp)}&deg`;
+        mintemp.innerHTML = `${Converttemp(data.main.temp_min)}`;
+        maxtemp.innerHTML = `${Converttemp(data.main.temp_max)}`;
+        feel.innerHTML = `${Converttemp(data.main.feels_like)}`;
+      });
+    }
+  };
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
---
+++ src/converttemp.js
@@ -0,0 +1,7 @@
+const Converttemp = (input) => {
+  const fahrenheit = Math.round((9 / 5) * input + 32);
+  return fahrenheit;
+};
+
+
+export default Converttemp;
